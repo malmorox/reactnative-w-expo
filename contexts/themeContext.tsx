30ae34871254c9421f548dcaa115b060265b4c25
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2025-10-29 12:30:18 +0100

    Add ThemeContext and ThemeProvider for theming

---
+++ contexts/themeContext.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from 'react';
+import { Colors } from './constants/Colors';
+import { useColorScheme } from 'react-native';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const systemScheme = useColorScheme();
+  const [themeName, setThemeName] = useState(systemScheme);
+
+  const theme = Colors[themeName];
+
+  const toggleTheme = () => setThemeName(themeName === 'light' ? 'dark' : 'light');
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
